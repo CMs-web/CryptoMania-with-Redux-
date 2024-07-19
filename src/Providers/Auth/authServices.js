92774e91c34a09ff2719a1f2cd46802c04f50230
Shivam Goswami <shivamgoswami.work@gmail.com>
2024-07-19 14:40:53 +0530

    Modify Auth

--- src/Providers/Auth/authServices.js
+++ src/Providers/Auth/authServices.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "/api/user";
+const API_URL = "https://auth-backend-93eo.onrender.com/api/user";
 
 const registerUser = async (formData) => {
     const response = await axios.post(API_URL + "/register", formData);
