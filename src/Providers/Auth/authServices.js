6674b33d2127752a92dbacf12c07789dd003c8dd
Shivam Goswami <shivamgoswami.work@gmail.com>
2024-07-19 14:58:44 +0530

    modify url

--- src/Providers/Auth/authServices.js
+++ src/Providers/Auth/authServices.js
@@ -1,21 +1,24 @@
 import axios from "axios";
 
-
-const API_URL = "https://authentication-2-qzge.onrender.com/api/user";
-
 const registerUser = async (formData) => {
-    const response = await axios.post(API_URL + "/register", formData);
-    localStorage.setItem("user", JSON.stringify(response.data));
-    return response.data
-}
+  const response = await axios.post(
+    "https://authentication-2-qzge.onrender.com/api/user/register",
+    formData
+  );
+  localStorage.setItem("user", JSON.stringify(response.data));
+  return response.data;
+};
 const loginUser = async (formData) => {
-    const response = await axios.post(API_URL + "/login", formData);
-    localStorage.setItem("user", JSON.stringify(response.data));
-    return response.data;
-}
+  const response = await axios.post(
+    "https://authentication-2-qzge.onrender.com/api/user/login",
+    formData
+  );
+  localStorage.setItem("user", JSON.stringify(response.data));
+  return response.data;
+};
 
 const authServices = {
-    registerUser,
-    loginUser,
-}
-export default authServices
+  registerUser,
+  loginUser,
+};
+export default authServices;
